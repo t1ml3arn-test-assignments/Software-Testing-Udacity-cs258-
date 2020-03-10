65c4ee23f372e1e1f0bf2f57726c6cb0259b070f
T1mL3arn <T1mL3arn@users.noreply.github.com>
2020-03-10 16:14:00 +1000

    lesson10 quiz 4 done

--- lesson10_exam/q4_code_coverage.py
+++ lesson10_exam/q4_code_coverage.py
@@ -22,6 +22,13 @@
 # False if the number of items in the collection will overfill
 # the Queue.
 
+# To run this quiz from command line, you need to:
+# 1. install python coverage library
+# 2. run it with command `coverage run --branch q4_code_coverage.py && coverage report && coverage html`
+    # 2.1 `--branch` checks branch coverage (by default it checks only statement coverage)
+    # 2.2 `report` shows report in the console output
+    # 2.3 `html` generates html report, you need to locate the report file and open it with a browser
+
 
 # Enhanced Queue class
 class Queue:
@@ -77,7 +84,7 @@ class Queue:
             self.head = 0
         return x 
 
-    def checkRep(self):
+    def checkRep(self): # pragma: no cover
         assert self.tail >= 0
         assert self.tail < self.max
         assert self.head >= 0
@@ -89,7 +96,84 @@ class Queue:
         if self.head == self.tail:
             assert (self.size==0) or (self.size==self.max)
 
+import random
 
 # Provide full statement and parameter value coverage of the Queue class
-def test():
-    pass
+def test(): # pragma: no cover
+    
+    # test empty que
+    q = Queue(100)
+    assert not q.full()
+    assert q.empty()
+    assert str(q) == '{}'
+    q.checkRep()
+
+    # test enque
+    q = Queue(3)
+    # cannot add not int or not bool or not string
+    assert q.enqueue(set()) is False
+    rand_int = random.randint(-1e8, 1e8)
+    assert q.enqueue(rand_int) is True
+    assert q.enqueue(False) is True
+    assert q.enqueue('hello') is True
+    # cannot enque into full que
+    assert q.enqueue(123) is False
+    assert q.enqueue(321) is False
+
+    # test deque
+    assert q.dequeue() == rand_int
+    assert q.dequeue() is False
+    assert q.dequeue() == 'hello'
+    # cannot deque empty que
+    assert q.dequeue() is None
+    assert q.dequeue() is None
+
+    # test empty and full
+    q = Queue(1)
+    q.enqueue(123)
+    assert not q.empty()
+    assert q.full() is True
+    q.dequeue()
+    q.dequeue()
+    assert q.empty() is True
+    assert not q.full()
+
+    # test clear
+    q = Queue(100)
+    for i in range(100):
+        q.enqueue(random.randrange(1e8))
+
+    q.clear()
+    assert q.empty() is True
+    # refullfill que again
+    for i in range(100):
+        q.enqueue(random.randrange(1e8))
+    assert q.full() is True
+
+    # test string repr
+    q = Queue(2)
+    q.enqueue(1)
+    q.enqueue('hello')
+    assert str(q) != '{}'
+    assert len(str(q)) > 5
+
+    # test enque all
+    listA = [1,2,1,3, True, False, 'foo', 'bar', 'foo']
+    listB = tuple(listA)
+    
+    assert Queue(len(listA)).enqueueall(listA) is True
+    assert Queue(len(listB)).enqueueall(listB) is True
+    # overfilling
+    assert Queue(1).enqueueall(listA) is False
+    assert Queue(1).enqueueall(listB) is False
+    # trying to enqueall with non-supported type
+    assert Queue(2).enqueueall(set([1,2])) is False
+
+    # trying to enqueall when list contains non-supported item
+    listC = [set(), 123]
+    q = Queue(10)
+    assert q.enqueueall(listC) is True
+    assert q.dequeue() == 123
+    assert q.empty() is True
+
+test()
